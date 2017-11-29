60d89d3ecbd6c84c80b8ce46b208b803a694c235
Your Name <you@example.com>
2017-11-28 23:30:30 -0800

    Columns for the query when the user searches for an artist or song

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,3 +1,4 @@
+$( document ).ready(function() {
 var Trackster = {};
 
 /*
@@ -13,5 +14,25 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+	$.ajax({
+	url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title + "&api_key="+ API_KEY + "&format=json",
+	datatype: 'jsonp',
+	success: function(){
+		console.log("sucess");
+	}
+});
 };
+
+/*
+	Adding some interativity to the button
+ */
+$('#search-button').click(function(){
+	var $item = $('.music-search').val();
+	Trackster.searchTracksByTitle($item);
+})
+const API_KEY = 'c5c83bce364f8f016a2400e48b06f348';
+
+
+
+	
+});
